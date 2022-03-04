7fa5cfadd53b771581cb59fba9ad99f6f833412e
LB9457 <98156444+LB9457@users.noreply.github.com>
2022-03-04 17:04:58 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,13 +15,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_crosshatch.mk \
-    $(LOCAL_DIR)/aosp_blueline.mk \
-    $(LOCAL_DIR)/aosp_crosshatch_hwasan.mk \
-    $(LOCAL_DIR)/aosp_blueline_hwasan.mk \
-    $(LOCAL_DIR)/vf/aosp_crosshatch_vf.mk \
+    $(LOCAL_DIR)/nortical_crosshatch.mk \
+    $(LOCAL_DIR)/nortical_blueline.mk \
+    $(LOCAL_DIR)/nortical_crosshatch_hwasan.mk \
+    $(LOCAL_DIR)/nortical_blueline_hwasan.mk \
+    $(LOCAL_DIR)/vf/nortical_crosshatch_vf.mk \
 
 COMMON_LUNCH_CHOICES := \
-    aosp_crosshatch-userdebug \
-    aosp_blueline-userdebug \
-    aosp_crosshatch_vf-userdebug
+    nortical_crosshatch-userdebug \
+    nortical_blueline-userdebug \
+    nortical_crosshatch_vf-userdebug
