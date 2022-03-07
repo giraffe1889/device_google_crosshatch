2aab924c2de8d932983bc6297404eebd442044e7
giraffe1889 <40524111+giraffe1889@users.noreply.github.com>
2022-03-07 19:36:34 +0800

    Update and rename nortical_crosshatch_vf.mk to potato_blueline_vf.mk

--- vf/potato_blueline_vf.mk
+++ vf/potato_blueline_vf.mk
@@ -30,4 +30,4 @@ PRODUCT_BUILD_VENDOR_BOOT_IMAGE := false
 
 $(call inherit-product, device/google/crosshatch/nortical_crosshatch.mk)
 
-PRODUCT_NAME := nortical_crosshatch_vf
+PRODUCT_NAME := potato_blueline_vf
