f719ecf5874fe6fc642a26dcd5dbd23ce7d17f24
giraffe1889 <40524111+giraffe1889@users.noreply.github.com>
2022-03-07 19:39:35 +0800

    Update potato_blueline_vf.mk

--- vf/potato_blueline_vf.mk
+++ vf/potato_blueline_vf.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-# Makefile for the system-only partial build of nortical_crosshatch.
+# Makefile for the system-only partial build of potato_blueline.
 # Used for creating a merged Vendor Freeze device.
 
 # Include VNDK v30, needed by the Android 11 vendor half.
@@ -28,6 +28,6 @@ PRODUCT_BUILD_VBMETA_IMAGE := false
 PRODUCT_BUILD_VENDOR_IMAGE := false
 PRODUCT_BUILD_VENDOR_BOOT_IMAGE := false
 
-$(call inherit-product, device/google/crosshatch/nortical_crosshatch.mk)
+$(call inherit-product, device/google/blueline/potato.mk)
 
 PRODUCT_NAME := potato_blueline_vf
